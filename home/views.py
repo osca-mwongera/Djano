54d8b0e44285f5830aceee7901107e01d4c4d3c3
Osca Mwongera <oscamwongera@gmail.com>
2023-07-12 09:18:12 +0200

    Fix rendering of the index page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,17 @@
+import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def homepage(request):
-    return render(request, 'home/index.html', content_type='')
+    # now = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
+    return render(
+        request=request, template_name='home/index.html', context=None, content_type=None, status=200, using=None
+    )
 
 
