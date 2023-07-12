ca6be1fe1e33b2b9ad55f776ac81618e63ae0845
Osca Mwongera <oscamwongera@gmail.com>
2023-07-12 15:59:37 +0200

    Refactor to check that django is ready before executing the scripts

--- Data/load_county_layer.py
+++ Data/load_county_layer.py
@@ -1,26 +1,34 @@
+import django
 import os
 
-from django.contrib.gis.utils import LayerMapping
+if __name__ == '__main__':
+    django.setup()
 
-from land.models import County
+    from django.contrib.gis.utils import LayerMapping
+
+    from land.models import County as landCounty
+    from houses.models import County as houseCounty
+
+    county_mapping = {
+        'objectid': 'OBJECTID',
+        'unit_area': 'UNIT_AREA',
+        'unit_perim': 'UNIT_PERIM',
+        'district': 'DISTRICT',
+        'count_field': 'COUNT_',
+        'county_nam': 'COUNTY_NAM',
+        'code': 'CODE',
+        'shape_leng': 'Shape_Leng',
+        'shape_area': 'Shape_Area',
+        'geom': 'MULTIPOLYGON',
+    }
+
+    shpFilePath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'County.shp'))
 
 
-county_mapping = {
-    'objectid': 'OBJECTID',
-    'unit_area': 'UNIT_AREA',
-    'unit_perim': 'UNIT_PERIM',
-    'district': 'DISTRICT',
-    'count_field': 'COUNT_',
-    'county_nam': 'COUNTY_NAM',
-    'code': 'CODE',
-    'shape_leng': 'Shape_Leng',
-    'shape_area': 'Shape_Area',
-    'geom': 'MULTIPOLYGON',
-}
+    def load_data(verbose=True):
+        lc_lm = LayerMapping(landCounty, shpFilePath, county_mapping, encoding='iso-8859-1', transform=False)
+        lc_lm.save(verbose=verbose, strict=True)
+        hc_lm = LayerMapping(houseCounty, shpFilePath, county_mapping, encoding='iso-8859-1', transform=False)
+        hc_lm.save(verbose=verbose, strict=True)
 
-shpFilePath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'County.shp'))
-
-
-def load_data(verbose=True):
-    lm = LayerMapping(County, shpFilePath, county_mapping, encoding='iso-8859-1', transform=False)
-    lm.save(verbose=verbose, strict=True)
+    load_data()
--- Data/load_district_layer.py
+++ Data/load_district_layer.py
@@ -1,19 +1,27 @@
+import django
 import os
 
-from django.contrib.gis.utils import LayerMapping
+if __name__ == '__main__':
+    django.setup()
 
-from houses.models import District
+    from django.contrib.gis.utils import LayerMapping
+
+    from houses.models import District as houseDistrict
+    from land.models import District as landDistrict
+
+    district_mapping = {
+        'distname': 'DISTNAME',
+        'geom': 'MULTIPOLYGON',
+    }
+
+    shpFilePath = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'ke_district_boundaries/ke_district_boundaries.shp'))
 
 
-district_mapping = {
-    'distname': 'DISTNAME',
-    'geom': 'MULTIPOLYGON',
-}
+    def load_data(verbose=True):
+        hd_lm = LayerMapping(houseDistrict, shpFilePath, district_mapping, encoding='iso-8859-1', transform=False)
+        hd_lm.save(verbose=verbose, strict=True)
+        ld_lm = LayerMapping(landDistrict, shpFilePath, district_mapping, encoding='iso-8859-1', transform=False)
+        ld_lm.save(verbose=verbose, strict=True)
 
-shpFilePath = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                           'ke_district_boundaries/ke_district_boundaries.shp'))
-
-
-def load_data(verbose=True):
-    lm = LayerMapping(District, shpFilePath, district_mapping, encoding='iso-8859-1', transform=False)
-    lm.save(verbose=verbose, strict=True)
+    load_data()
